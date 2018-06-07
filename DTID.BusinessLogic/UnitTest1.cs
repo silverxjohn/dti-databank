d75432fa8c47da2b444a4bfb07ac0c3ee3a1c8f2
John Aldrin Plata <king.aldrin31@gmail.com>
2018-06-07 14:32:30 +0800

    adds test solution and change database connection

---
+++ DTID.BusinessLogic/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DTID.BusinessLogic
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
