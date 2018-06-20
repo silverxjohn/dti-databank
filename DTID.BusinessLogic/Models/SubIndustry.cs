03ba00b75183a469eee1f083f9bc8bcc9ab23e6e
Claud Montaril <claudmontaril@gmail.com>
2018-06-20 10:17:06 +0800

    additional models
    Industries

---
+++ DTID.BusinessLogic/Models/SubIndustry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTID.BusinessLogic.Models
+{
+    public class SubIndustry
+    {
+        public int ID { get; set; }
+        public Industry Industry { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
