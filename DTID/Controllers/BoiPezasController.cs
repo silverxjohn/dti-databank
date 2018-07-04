03139ed9596a9924efe044a4774de08d16e9cdc5
Felix Jacob <felixborlongan@gmail.com>
2018-07-04 11:29:40 +0800

    download table for annual boi-peza

--- DTID/Controllers/BoiPezasController.cs
+++ DTID/Controllers/BoiPezasController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DTID.BusinessLogic.Models;
 using DTID.Data;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
 
 namespace DTID.Controllers
 {
@@ -15,10 +19,12 @@ namespace DTID.Controllers
     public class BoiPezasController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public BoiPezasController(ApplicationDbContext context)
+        public BoiPezasController(ApplicationDbContext context, IHostingEnvironment hostingEnvironment)
         {
             _context = context;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         // GET: api/BoiPezas
@@ -121,6 +127,75 @@ namespace DTID.Controllers
         private bool BoiPezaExists(int id)
         {
             return _context.BoiPezas.Any(e => e.ID == id);
+        }
+
+        [HttpGet("Download")] //api/BoiPezas/Download
+        public async Task<IActionResult> OnPostExport()
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = @"excels/demo.xlsx";
+            FileInfo file = new FileInfo(System.IO.Path.Combine(sWebRootFolder, sFileName));
+            var memory = new MemoryStream();
+            using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet boiSheet = workbook.CreateSheet("BOI");
+                IRow boiHead = boiSheet.CreateRow(0);
+                boiHead.CreateCell(0).SetCellValue("Statistics on BOI-Approved Investments");
+
+                var boiPezas = _context.BoiPezas.Include(boiPeza => boiPeza.Year).GroupBy(boiPeza => boiPeza.YearId).Select(boiPeza => boiPeza.First());
+
+                IRow boiYears = boiSheet.CreateRow(1);
+                boiYears.CreateCell(0).SetCellValue(boiPezas.First().Year.Name + "-" + boiPezas.Last().Year.Name);
+
+                IRow boiLabels = boiSheet.CreateRow(3);
+                boiLabels.CreateCell(0).SetCellValue("Year");
+                boiLabels.CreateCell(1).SetCellValue("Amount (in Billion PHP)");
+
+                var i = 4;
+
+                foreach (var boi in boiPezas)
+                {
+                    IRow boiRow = boiSheet.CreateRow(i);
+
+                    boiRow.CreateCell(0).SetCellValue(Int32.Parse(boi.Year.Name));
+                    boiRow.CreateCell(1).SetCellValue(boi.BOI);
+
+                    i++;
+                }
+
+                ISheet pezaSheet = workbook.CreateSheet("PEZA");
+                IRow pezaHead = pezaSheet.CreateRow(0);
+                pezaHead.CreateCell(0).SetCellValue("Statistics on PEZA-Approved Investments");
+
+                IRow pezaYears = pezaSheet.CreateRow(1);
+                pezaYears.CreateCell(0).SetCellValue(boiPezas.First().Year.Name + "-" + boiPezas.Last().Year.Name);
+
+                IRow pezaLabels = pezaSheet.CreateRow(3);
+                pezaLabels.CreateCell(0).SetCellValue("Year");
+                pezaLabels.CreateCell(1).SetCellValue("Amount (in Billion PHP)");
+
+                var y = 4;
+
+                foreach (var peza in boiPezas)
+                {
+                    IRow pezaRow = pezaSheet.CreateRow(y);
+
+                    pezaRow.CreateCell(0).SetCellValue(Int32.Parse(peza.Year.Name));
+                    pezaRow.CreateCell(1).SetCellValue(peza.Peza);
+
+                    y++;
+                }
+
+                workbook.Write(fs);
+            }
+            using (var stream = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            var newFileName = "BOI_PEZA" + DateTime.Now.ToString("MM-dd-yyyy_hh:mm_tt") + ".xlsx";
+            return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", newFileName);
         }
     }
 }
