abd872f60fac33e68adb10375e174e01d2fc8c7e
Felix Jacob <felixborlongan@gmail.com>
2018-07-06 17:16:41 +0800

    added backbone for attachment upload, download, delete and interpretation

--- DTID/Data/ApplicationDbContext.cs
+++ DTID/Data/ApplicationDbContext.cs
@@ -39,6 +39,8 @@ namespace DTID.Data
         public DbSet<CannedIndicator> CannedIndicator { get; set; }
         public DbSet<BoiPeza> BoiPezas { get; set; }
         public DbSet<IndicatorData> IndicatorDatas { get; set; }
+        public DbSet<Interpretation> Interpretations { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
