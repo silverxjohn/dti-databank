1a0c7a93a56cb370a5c1d4cb5826885039db1563
Felix Jacob <felixborlongan@gmail.com>
2018-07-18 10:59:51 +0800

    arranged logs in descending order according to datecreated

--- DTID/Controllers/LogsController.cs
+++ DTID/Controllers/LogsController.cs
@@ -27,7 +27,7 @@ namespace DTID.Controllers
         [HttpGet]
         public IEnumerable<Log> GetLogs()
         {
-            return _context.Logs.Include(log => log.Users).ToList();
+            return _context.Logs.Include(log => log.Users).OrderByDescending(logs => logs.DateCreated).ToList();
         }
 
         // GET: api/Logs/5
